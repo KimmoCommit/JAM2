ec8536e92e59a631e1c20e09e9c865008918da13
kimmocommit <kimmocommit@kimmocommit-HP-EliteBook-2570p.(none)>
2014-08-17 02:05:03 +0300

    added color and new structure

---
+++ javascripts/Resizor.js
@@ -0,0 +1,50 @@
+function Resizor(){
+
+    var winwidth = $(window).width();
+    var winheight = $(window).height();
+
+    this.setWinwidth = function(w){
+        winwidth = s;
+    }
+
+    this.getWinwidth = function(){
+        return winwidth;
+    }
+
+    this.setWinheight = function(h){
+        winheight = h;
+    }
+
+    this.getWinheight = function(){
+        return winheight;
+    }
+}
+
+Resizor.prototype.resizeHeight = function(div){
+    var winheight = this.getWinheight() - 200;
+    this.div = div;
+    $(div).css({'min-height': winheight + 'px'});
+}
+
+Resizor.prototype.resizeWidth = function(div){
+    var winwidth = this.getWinwidth();
+    this.div = div;
+    $(div).css({'width': winwidth + 'px'});
+}
+
+Resizor.prototype.resizeBoth = function(div){
+    var winwidth = this.getWinwidth();
+    var winheight = this.getWinheight();
+    this.div = div;
+    $(div).css({'min-height': winheight + 'px'});
+    $(div).css({'width': winwidth + 'px'});
+    $(div).css({'max-width': winwidth + 'px'});
+
+}
+
+Resizor.prototype.resizeToDivHeight = function(div,model){
+    this.div = div;
+    this.model = model;
+    var modelHeight = $(model).height();
+    $(div).css({'min-height': modelHeight + 'px'});
+}
